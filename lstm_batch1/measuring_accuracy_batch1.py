c21d3ef853f123e441b15b25f2699be9b613fb3d
Jona Bosman <47357431+JonaBenja@users.noreply.github.com>
2019-06-21 12:03:14 +0200

    Update measuring_accuracy_batch1.py

--- lstm_batch1/measuring_accuracy_batch1.py
+++ lstm_batch1/measuring_accuracy_batch1.py
@@ -30,10 +30,10 @@ for sound in test_data:
 
 print(len(test_data1))
 
-model = load_model('../models/BEST_lstm_batch1.h5')
+model = load_model('../models/17-06lstm_batch1.h5')
 prediction = []
 for i in range(len(test_data1)):
-    [test] = model.predict_on_batch(test_data1[i])
+    [test] = model.predict(test_data1[i], batch_size = 1)
     sound = []
     for frame in test:
         frame = frame.tolist()
@@ -54,7 +54,7 @@ for sound in prediction:
         num_frame += 1
         node = frame.index(max(frame))
         label = test_labels[num_sound-1][num_frame-1]
-        if node != label[0]-1:
+        if node != label[0]:
             errors += 1
 
 print("Total frames:", total_frames)
@@ -74,6 +74,8 @@ gross_errors_node = 0
 gross_errors_voicing = 0
 too_high = 0
 too_low = 0
+voiceless = 0
+voiced = 0
 
 # measure error percentage like YIN does
 for sound in prediction:
@@ -84,32 +86,60 @@ for sound in prediction:
         test_frames += 1
         num_frame += 1
         node = frame.index(max(frame))
-        candidate = (test_data[num_sound-1][num_frame-1][node-COR]*100)
 
-        if (label[0] == 0 and node != 0) or (label[0] != 0 and node == 0):
-            gross_errors_voicing += 1
+        if label[0] != 0:
+            if node == 0:
+                voiceless += 1
+                """
+                print("************************")
+                print("Prediction:", frame)
+                print("Node:", node)
+                print("Label:", label)
+                """
 
-        elif label[0] != 0 and node != 0:
-            if abs(candidate - label[1]) / label[1] > 0.2:
-                gross_errors_freq += 1
-                if candidate > label[1]:
-                    too_high += 1
-                elif candidate < label[1]:
-                    too_low += 1
+            else:
+                candidate = test_data[num_sound-1][num_frame-1][node-COR]*100
+                if abs(candidate - label[1]) / label[1] > 0.2:
+                    print("************************")
+                    print("Prediction:", frame)
+                    print("Node:", node)
+                    print("Candidate:", candidate)
+                    print("Frame:", test_data[num_sound-1][num_frame-1])
+                    print("Label:", label)
 
-            if node != label[0]-1:
-                gross_errors_node += 1
+                    if candidate > label[1]:
+                        too_high += 1
+                    elif candidate < label[1]:
+                        too_low += 1
+        # voiceless
+        else:
+            if node != 0:
+                voiced += 1
+                """
+                print("************************")
+                print("Prediction:", frame)
+                print("Node:", node)
+                print("Label:", label)
+                """
 
+
+gross_errors_freq = too_high + too_low
+gross_errors_voicing = voiceless + voiced
 gross_errors = gross_errors_freq + gross_errors_voicing
 
 print("The number of test frames is: ", test_frames)
 print("The number of gross errors is: ", gross_errors)
 print("The number of too high gross errors is: ", too_high)
 print("The number of too low gross errors is: ", too_low)
+print("The number of wrong voiced gross errors is: ", voiced)
+print("The number of wrong voiceless gross errors is: ", voiceless)
 print("The number of gross errors freq is: ", gross_errors_freq)
 print("The number of gross errors voicing is: ", gross_errors_voicing)
-print("The percentage of gross errors freq is: ", round(gross_errors_freq/test_frames*100, 2), "%")
 print("The percentage of gross errors voicing is: ", round(gross_errors_voicing/test_frames*100, 2), "%")
+print("The percentage of wrong voiced gross errors is: ", round(voiced/gross_errors_voicing*100, 2), "%")
+print("The percentage of wrong voiceless gross errors is: ", round(voiceless/gross_errors_voicing*100, 2), "%")
+print("The percentage of gross errors freq is: ", round(gross_errors_freq/test_frames*100, 2), "%")
 print("The percentage of too high gross errors is: ", round(too_high/gross_errors_freq*100, 2), "%")
 print("The percentage of too low gross errors is: ", round(too_low/gross_errors_freq*100, 2), "%")
 print("The percentage of (all) gross errors is: ", round(gross_errors/test_frames*100, 2), "%")
+print("The accuracy is: ", round(100-(gross_errors/test_frames*100), 2), "%")
